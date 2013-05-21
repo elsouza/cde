512232692a6712305c33eeb5d3010aa24acc2ee0
elsouza <eliaslsouza@gmail.com>
2013-05-21 16:23:43 -0300

    Cdewebservices - busca de todos os dashboards

--- server/plugin/src/pt/webdetails/cdf/dd/ws/CdeWebServices.java
+++ server/plugin/src/pt/webdetails/cdf/dd/ws/CdeWebServices.java
@@ -1,9 +1,36 @@
 package pt.webdetails.cdf.dd.ws;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import net.sf.json.JSONObject;
+
+import org.apache.commons.jxpath.JXPathContext;
+
+import pt.webdetails.cdf.dd.render.RenderComponents;
+import pt.webdetails.cdf.dd.util.JsonUtils;
+
 
 public class CdeWebServices {
 
     public String test(String echo) {
         return "This is my echo: " + (echo == null ? "null" : echo);
     }
+
+	public String getScriptForComponentId(String dashboardName, String componentId) {
+		
+		try {
+			FileInputStream streamDashboard = new FileInputStream(dashboardName);
+			final JSONObject json = (JSONObject) JsonUtils.readJsonFromInputStream(streamDashboard);
+			final JXPathContext doc = JXPathContext.newContext(json);
+			
+			return new RenderComponents().render(doc, ""); //TODO oq é o ALIAS?
+			
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		
+	}
 }
